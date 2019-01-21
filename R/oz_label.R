342e642ebfa81789b11f0315222666825cc1e2ac
Adam H. Sparks <adamhsparks@gmail.com>
2019-01-22 09:42:03 +1000

    Add documentation for new data

---
+++ R/oz_label.R
@@ -0,0 +1,9 @@
+#' Base Map of Queensland and New South Wales for Labelling
+#'
+#' Data to provide a basemap for reporting, this data provides a layer with
+#' state outlines and is suitable for labelling.
+#'
+#' @format A \code{\link[sf]{sf}} object.
+#' @seealso \code{\link{oz_outline}}
+#'
+"oz_label"
---
+++ R/oz_outline.R
@@ -0,0 +1,9 @@
+#' Outline of Queensland and New South Wales
+#'
+#' Data to provide a map for reporting, this data provides a layer with
+#' a federation-level outline for use on top of \code{\link{oz_label}}.
+#'
+#' @format A \code{\link[sf]{sf}} object.
+#' @seealso \code{\link{oz_label}}
+#'
+"oz_outline"
